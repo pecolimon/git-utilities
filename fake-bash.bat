96b71532e8975b6afe232f8535cf15255d8a83ed
pecolimon <33423016+pecolimon@users.noreply.github.com>
2017-11-06 13:21:56 -0500

    Update fake-bash.bat

--- fake-bash.bat
+++ fake-bash.bat
@@ -1,6 +1,6 @@
 @echo off
 :: From https://github.com/wilsonmar/git-utilities/fake-bash.bat
-:: Create macros to emulate Linux/Mac BASH commands:
+:: Create handy macros to emulate Linux/Mac BASH commands:
 
 DOSKEY ls=dir /X
 DOSKEY pwd=chdir
